d1f7d1e2267ccde67885a9d4b5e35a01c2092a67
Geoff Little <fifosine@gmail.com>
2014-06-27 15:12:10 -0400

    Initial commit.

---
+++ src/my_lists/handler.clj
@@ -0,0 +1,25 @@
+(ns my-lists.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [my-lists.routes.home :refer [home-routes]]))
+
+(defn init []
+  (println "my-lists is starting"))
+
+(defn destroy []
+  (println "my-lists is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (routes home-routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
+
+
